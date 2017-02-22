de9caa5825db608cd188fd884a204b2ef60403d3
Jonny Holupchinski <jonholup@gmail.com>
2017-02-22 11:48:14 -0600

    fish communication works

--- server/app.js
+++ server/app.js
@@ -5,4 +5,11 @@ var app = express(); //returns an object with a bunch of function properties (in
 
 app.use(express.static('server/public')); // incoming request for a static file? look in public folder
 
+var fishiesList = [{name: 'walleye'}, {name: 'pike'}]
+
+app.get('/fish', function(req, res){
+  console.log('req: ', req);
+  res.send(fishiesList);
+});
+
 app.listen(5000);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,11 @@
 console.log('sourced');
-
 $(document).ready(function(){
   console.log('jquery here');
-
+  $.ajax({
+    type: 'GET',
+    url: '/fish',
+    success: function(response){
+      console.log('response: ', response);
+    }
+  })
 });
