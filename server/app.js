40caa36c30d06336a7d06ba10cdd1800f044dbb1
Jonny Holupchinski <jonholup@gmail.com>
2017-02-22 15:22:05 -0600

    get requests working

--- server/app.js
+++ server/app.js
@@ -5,11 +5,29 @@ var app = express(); //returns an object with a bunch of function properties (in
 
 app.use(express.static('server/public')); // incoming request for a static file? look in public folder
 
-var fishiesList = [{name: 'walleye'}, {name: 'pike'}]
+var fishiesList = [{name: 'walleye'}, {name: 'shrimp'}, {name: 'muskellunge'}]
 
 app.get('/fish', function(req, res){
   console.log('req: ', req);
   res.send(fishiesList);
 });
+// handle the request for the first fish
+app.get('/fish/first', function(req, res){
+  res.send(fishiesList[0]);
+});
+// handle request for the name of the first fish
+app.get('/fish/first/name', function(req, res){
+  res.send(fishiesList[0].name);
+});
 
+// handle request for last fish
+app.get('/fish/last', function(req, res){
+  var lastIndex = fishiesList.length - 1;
+  res.send(fishiesList[lastIndex]);
+});
+// handle request for the name of the last fish
+app.get('/fish/last/name', function(req, res){
+  var lastIndex = fishiesList.length - 1;
+  res.send(fishiesList[lastIndex].name);
+});
 app.listen(5000);
