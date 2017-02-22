3a69a9b72a91539da9d2919075070a318717fc34
Jonny Holupchinski <jonholup@gmail.com>
2017-02-22 11:36:42 -0600

    added folders

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
-console.log('Starting up the server <- this is a lie for now');
+console.log('Starting up the server');
+
+var express = require('express'); // bring in express library
+var app = express(); //returns an object with a bunch of function properties (including get and post)
+
+app.use(express.static('server/public')); // incoming request for a static file? look in public folder
+
+app.listen(5000);
